a413732592af0b1ba0f9c59fdd5ee00be98a0510
CLAUDISNEI <claudisneibello@gmail.com>
2020-09-10 09:14:37 -0300

    Projeto Iniciado

---
+++ src/application/Exemplo1.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Produto;
+
+public class Exemplo1 {
+
+	public static void main(String[] args) {
+		
+		List<Produto> list = new ArrayList<>();
+		list.add(new Produto("Tv", 900.00));
+		list.add(new Produto("Mouse", 50.00));
+		list.add(new Produto("Tablet", 350.50));
+		list.add(new Produto("HD Case", 80.90));
+		
+		
+
+	}
+
+}
---
+++ src/model/service/ServicoProduto.java
@@ -0,0 +1,19 @@
+package model.service;
+
+import java.util.List;
+
+import model.entities.Produto;
+
+public class ServicoProduto {
+	
+	public double filtroSoma(List<Produto> list) {
+		double soma = 0;
+		for(Produto p: list) {
+			if(p.getNome().charAt(0)=='T') {
+				soma =+ p.getPreco();
+			}
+		}
+		
+		return soma;
+	}
+}
